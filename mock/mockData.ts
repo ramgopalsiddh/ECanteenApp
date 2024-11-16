ec91961508fda10843c8cf3edaa4453ee7bed275
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-11-16 17:14:53 +0530

    Added Mock data

---
+++ mock/mockData.ts
@@ -0,0 +1,9 @@
+const mockMenu = [
+    { id: "1", name: "Pizza", price: 10 },
+    { id: "2", name: "Burger", price: 8 },
+    { id: "3", name: "Juice", price: 5 },
+    { id: "4", name: "Sandwich", price: 7 },
+  ];
+  
+  export default mockMenu;
+  
